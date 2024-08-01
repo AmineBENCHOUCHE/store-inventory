12d79d4d74b92e35153b70da9038da3d94131b92
Amine BENCHOUCHE <benchouche.amine@outlook.com>
2024-08-01 14:29:24 +0200

    Add themeprovider

---
+++ src/theme.tsx
@@ -0,0 +1,16 @@
+"use client"
+// theme.js
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#009dff',
+    },
+    secondary: {
+      main: '#e8ffff',
+    },
+  },
+});
+
+export default theme;
