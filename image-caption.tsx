8e81a21bf8c8cc183a3740a2ecd3dc4a78648be9
Amine BENCHOUCHE <benchouche.amine@outlook.com>
2024-08-02 19:06:29 +0200

    Update ima-caption file

--- image-caption.tsx
+++ image-caption.tsx
@@ -2,32 +2,27 @@
 //Import the google cloud client library 
 import vision from '@google-cloud/vision';
 
-const analyzeImage = async (image: string) => {
+const analyzeImage = async (image: HTMLCanvasElement) => {
   try {
     // Validate image format (PNG or JPEG)
-    if (!image.match(/\.(png|jpg|jpeg)$/i)) {
-      console.error({ error: 'Invalid image format. Please provide a PNG or JPEG file.' });
-      return "Invalid image format. Please provide a PNG or JPEG file.";
-    }
+    const imageData = image.toDataURL('image/png'); // Change to 'image/jpeg' if needed
 
     const client = new vision.ImageAnnotatorClient({
-      keyFilename: './API_KEY.json' // replace with your service account key file path
+      keyFilename: './API_KEY.json'
     });
 
     // Get the file path of the image to analyze
-    const results = await client.labelDetection(image)
-      .then(results => {
-        const labels = results[0].labelAnnotations;
-        // console.log('Labels:');
-        if (labels) {
-          return labels[0]?.description;
-        } else {
-          console.error({ error: 'No labels detected in the image' });
-          return null;
-        }
+    const results = await client.labelDetection(imageData)
 
-        //labels.forEach(label => console.log(label.description));
-      })
+    const labels = results[0].labelAnnotations;
+    // console.log('Labels:');
+    if (labels) {
+      return labels[0]?.description;
+    } else {
+      console.error({ error: 'No labels detected in the image' });
+      return null;
+    }
+      
 
   } catch (error: any) { console.error('Error' + error.message); }
 
